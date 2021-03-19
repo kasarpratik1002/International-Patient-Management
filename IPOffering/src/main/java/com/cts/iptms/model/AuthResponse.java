687adccede1a1d699fe35b8de2205c779c32bb62
Sarvadnya <Sarvadnya@host.docker.internal>
2021-03-19 17:13:54 +0530

    Updated Model Classes

---
+++ IPOffering/src/main/java/com/cts/iptms/model/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.cts.iptms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+	private String uid;
+	private String name;
+	private boolean isValid;
+}
---
+++ IPOffering/src/main/java/com/cts/iptms/model/UserDetail.java
@@ -0,0 +1,10 @@
+package com.cts.iptms.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDetail {
+	private String name;
+}
